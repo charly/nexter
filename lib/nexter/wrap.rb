60a2dca2984f9f6d06576f1bd655af15951aff6c
Charles Sistovaris <charlysisto@gmail.com>
2015-12-28 12:52:19 +0100

    moved Model responsability out of Wrap

--- lib/nexter/wrap.rb
+++ lib/nexter/wrap.rb
@@ -1,17 +1,11 @@
 module Nexter
   class Wrap
-
     # the current model & the scope
-    attr_reader :model, :relation
-
-    # extracted values from the relation
-    attr_reader :order_values, :associations
+    attr_reader :model
 
     def initialize(relation, model)
-      @relation = relation
-      @model = model
-      @order_values = parse_order( relation.order_values )
-      @associations = relation.includes_values
+      order_values = parse_order( relation.order_values )
+      @model = Model.new(model, relation)
     end
 
     # TODO : let user determine which strategy to choose:
@@ -25,41 +19,16 @@ module Nexter
     end
 
     def after
-      query = Query.new(map_column_values, :next)
+      query = Query.new(model.values, :next)
       relation.where( query.wheres.join(' OR ') )
     end
 
     def before
-      query = Query.new(map_column_values, :previous)
+      query = Query.new(model.values, :previous)
       relation.where( query.wheres.join(' OR ') ).
               reorder( query.reorders.join(", ") )
     end
 
-    def map_column_values
-      @column_values ||= @order_values.map do |column|
-        {col: column[0], val: value_of(column[0]), dir: column[1]}
-      end
-    end
-
-  private
-    def value_of(cursor)
-      splits = cursor.split(".")
-      result = if splits.first == model.class.table_name || splits.size == 1
-        model.send(splits.last) if model.respond_to?(splits.last)
-      else
-        asso = model.class.reflections.keys.grep(/#{splits.first.singularize}/).first
-        asso = model.send(asso) and asso.send(splits.last)
-      end
-    end
-
-    # helper to turn mixed order attributes to a consistant
-    def parse_order(array)
-      array.join(",").split(",").map(&:strip).map do |column|
-        splits = column.split(" ").map(&:strip).map(&:downcase)
-        splits << "asc" if splits.size == 1
-        splits
-      end
-    end
 
   end
 end
--- spec/nexter/wrap_spec.rb
+++ spec/nexter/wrap_spec.rb
@@ -4,102 +4,14 @@ describe Nexter::Wrap do
   let(:relation) {Relation.new.tap{|r| r.order_values=["authors.name","genre","title"]}}
   let(:book)     {Book.new("novel", "nabokov", "ada")}
 
-  describe "#map_column_values", focus: true do
+  describe "#after" do
 
-    it "should create hashes of attributes out of the order values" do
+    it "should create a realtion" do
+      skip
       nexter = Nexter::Wrap.new(relation, book)
 
-      expect(nexter.map_column_values).to eq(
-        [ {col: "authors.name",  val: "nabokov", dir: "asc"},
-          {col: "genre",         val: "novel",   dir: "asc"},
-          {col: "title",         val: "ada",     dir: "asc"}
-        ])
+      expect(nexter.after).to eq("dfgjk")
     end
   end
-
-  context "no missing values", broken: true do
-    let(:relation) { Relation.new.tap {|rel| rel.order_values=["authors.name", "title"]} }
-    let(:book)     { Book.new("novel", "nabokov", "ada") }
-
-    describe "#wheres" do
-      it "has the right SQL condition" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.after
-
-        expect(nexter.wheres[0]).to eq("(authors.name = 'nabokov' AND title > 'ada')")
-      end
-    end
-
-    describe "#reorders" do
-      it "has the right SQL condition" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.before
-
-        expect(nexter.wheres[0]).to eq("(authors.name = 'nabokov' AND title < 'ada')")
-      end
-
-      it "has the right SQL order by" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.before
-
-        expect(nexter.reorders[0]).to eq(" authors.name DESC")
-      end
-    end
-  end
-
-  context "nil values", broken: true do
-    let(:relation) {Relation.new.tap {|r| r.order_values=["genre", "title", "id"]} }
-
-    describe "#wheres with Book#genre IS NULL" do
-      let(:book) { Book.new(nil, "nabokov", "Ada") }
-
-      it "has the right SQL condition" do
-        skip
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.after
-
-        expect(nexter.wheres).to eq("(genre IS NULL AND title > 'Ada')")
-      end
-    end
-
-    describe "#wheres with Book#title IS NULL" do
-      let(:book) { Book.new("novel", "nabokov", nil) }
-
-      it "has the right SQL condition for NEXT" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.after
-
-        expect(nexter.wheres[0]).to \
-          eq("(genre = 'novel' AND title IS NULL AND id > '#{book.id}')")
-
-        expect(nexter.wheres[1]).to \
-          eq("(  genre > 'novel')")
-      end
-
-
-      it "has the right SQL condition for PREVIOUS" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.before
-
-        expect(nexter.wheres[0]).to \
-          eq("(genre = 'novel' AND title IS NULL AND id < '#{book.id}')")
-
-        expect(nexter.wheres[1]).to \
-          eq("(  genre > 'novel')")
-      end
-    end
-
-    describe "#after with Book#title IS NULL" do
-      let(:book) { Book.new("novel", "nabokov", nil) }
-
-      it "has the right SQL condition" do
-        nexter = Nexter::Wrap.new(relation, book)
-        nexter.after
-
-        expect(nexter.wheres[0]).to \
-          eq("(genre = 'novel' AND title IS NULL AND id > '#{book.id}')")
-      end
-    end
-  end #contextr
 
 end
